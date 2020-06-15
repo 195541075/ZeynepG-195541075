a494c724491a7e8efdb2e565771dd043f582e247
195541075 <65676812+195541075@users.noreply.github.com>
2020-06-15 23:50:56 +0300

    Add files via upload

---
+++ 195541075_Zeynep_GEYİK/OKUL PROJESİ/BonusProje/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BonusProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            FrmÖğrenciNotlar fr = new FrmÖğrenciNotlar();
+            fr.numara = textBox1.Text;
+            fr.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 195541075_Zeynep_GEYİK/OKUL PROJESİ/BonusProje/FrmKulup2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace BonusProje
+{
+    public partial class FrmKulup2 : Form
+    {
+        public FrmKulup2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=LAPTOP-M253MUUE\SQLEXPRESS;Initial Catalog=BonusOkul;Integrated Security=True");
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT *FROM TBLKULUPLER", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
